8d0043cdfde7ff2a6ef0848198847a47b1df2781
AlejoCasti <acastiblanco8@gmail.com>
2020-07-12 19:06:44 -0500

    [FIX] bug to build

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
+  apiURL: 'https://alw-lab.herokuapp.com/',
   mapboxKey:'pk.eyJ1IjoiYWxlam9jYXN0aSIsImEiOiJja2NqNDNzOGYxNDJjMzBxeXFnemgwaDQyIn0.HZGbm61wWwSa475ajjHFHg'
 };
